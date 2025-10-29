940688736deb2f93e15662029aae50471abb91a9
Ansh <anshyadav9657@gmail.com>
2025-10-29 21:04:46 +0530

    update

--- frontend/school-project/src/components/Chatbot.jsx
+++ frontend/school-project/src/components/Chatbot.jsx
@@ -19,7 +19,7 @@ function Chatbot() {
 
       setDisabled(true)
 
-      const response = await fetch('http://127.0.0.1:8000/chatbot', { 
+      const response = await fetch('https://school-project-gm8t.onrender.com/chatbot', { 
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
